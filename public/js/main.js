59d86d0c8110e990b195e5f9dec91102b307f1c2
aniket384 <aniketmishra384@gmail.com>
2021-04-13 21:18:30 +0530

    Update

--- public/js/main.js
+++ public/js/main.js
@@ -8,12 +8,27 @@ const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
 
-function createSpriteLayer(sprites, pos){
+function createSpriteLayer(entity){
     return function drawSpriteLayer(context){
-        for (let i = 0; i < 20; ++i){
-            sprites.draw('idle', context, pos.x + i*16, pos.y);
-        }
+            entity.draw(context);
     };
+}
+
+class Vec2 {
+    constructor(x, y){
+        this.set(x, y);
+    }
+    set(x, y){
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Entity{
+    constructor(){
+        this.pos = new Vec2(0,0);
+        this.vel = new Vec2(0,0);
+    }
 }
 
 Promise.all([
@@ -27,18 +42,29 @@ Promise.all([
     const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
 
     comp.layers.push(backgroundLayer);
-    const pos ={
-        x: 0,
-        y: 0,
-    };
 
-    const spriteLayer = createSpriteLayer(marioSprite, pos);
+    const gravity = 0.5;
+
+    const mario = new Entity();
+    mario.pos.set(64, 180);
+    mario.vel.set(2, -10);
+
+
+    mario.draw = function drawMario(context) {
+        marioSprite.draw('idle', context, this.pos.x, this.pos.y);
+    }
+    mario.update = function updateMario(){
+        this.pos.x += this.vel.x;
+        this.pos.y += this.vel.y;
+    }
+
+    const spriteLayer = createSpriteLayer(mario);
     comp.layers.push(spriteLayer);
 
     function update(){
         comp.draw(context);
-        pos.x += 2;
-        pos.y += 2;
+        mario.update();
+        mario.vel.y += gravity;
         requestAnimationFrame(update);
     }
 
